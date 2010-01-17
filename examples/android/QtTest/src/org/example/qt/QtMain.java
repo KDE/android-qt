4091e046b7d86424dc65572bd6ccb2d2b3418e92
BogDan Vatra <bog_dan_ro@yahoo.com>
2010-01-17 23:48:16 +0200

    More gui work

--- examples/android/QtTest/src/org/example/qt/QtMain.java
+++ examples/android/QtTest/src/org/example/qt/QtMain.java
@@ -14,6 +14,7 @@ public class QtMain extends Activity {
     public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.main);
 		try
 		{
 			if (null==getLastNonConfigurationInstance())
