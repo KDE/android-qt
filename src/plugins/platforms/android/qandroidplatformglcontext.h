99bfd4928ac9000ec037d35e7973a9de947c7ba7
BogDan Vatra <bog_dan_ro@yahoo.com>
2010-08-05 22:51:50 +0800

    Fix android compialtion

--- src/plugins/platforms/android/qandroidplatformglcontext.h
+++ src/plugins/platforms/android/qandroidplatformglcontext.h
@@ -12,8 +12,10 @@ public:
     virtual void doneCurrent();
     virtual void swapBuffers();
     virtual void* getProcAddress(const QString& procName);
+    virtual QPlatformWindowFormat platformWindowFormat() const;
 private:
     WId     m_surfaceId;
+    QPlatformWindowFormat m_platformWindowFormat;
 };
 
 #endif // QANDROIDCONTEXT_H
