dd135773d919748290b88370d6abe29190f5e43b
mingw-android <mingw.android@gmail.com>
2011-03-16 02:12:35 +0000

    Added a build script.

---
+++ build.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Warning :: this will take ages.
+
+# Execute this from a folder where you want to build Qt for Android
+
+# TODO :: Add options for avr and 64bit.
+
+CLEAN=1
+CONFIGURE=1
+BUILDRELEASE=0
+BUILDDEBUG=0
+
+while getopts "p:c:q:d:r:" arg; do
+	echo "arg is $arg $OPTARG"
+	case $arg in
+		p)
+			`dirname $0`/build-mingw.sh -p
+			exit 0
+			;;
+		c)
+			CLEAN=$OPTARG
+			;;
+		q)
+			CONFIGURE=$OPTARG
+			;;
+		r)
+			BUILDRELEASE=$OPTARG
+			;;
+		d)
+			BUILDDEBUG=$OPTARG
+			;;
+	esac
+done
+
+if [ "$CLEAN" == "1" ]; then
+	CONFIGURE=1
+fi
+
+# Rationale for the below is ::
+# Static probably doesn't work.
+# RTTI is probably not needed.
+# Exceptions are needed for QtCreator (botan).
+
+`dirname $0`/build-mingw.sh 
+
+if [ "$BUILDRELEASE" == "1" ]; then
+	`dirname $0`/build-mingw.sh -c $CLEAN -q $CONFIGURE -h 1 -x 1 -r 1 -d 0 -m 1 -o C:/Qt/4.7.2-official -s /usr/Qt/Git -i C:/Necessitas/4.8.0
+fi
+if [ "$BUILDDEBUG" == "1" ]; then
+	`dirname $0`/build-mingw.sh -c $CLEAN -q $CONFIGURE -h 1 -x 1 -r 0 -d 1 -m 1 -o C:/Qt/4.7.2-official -s /usr/Qt/Git -i C:/Necessitas/4.8.0
+fi
