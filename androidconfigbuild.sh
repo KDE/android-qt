98c94df38e3fae93cb70a2b6814c9886c94cfb8b
BogDan Vatra <bog_dan_ro@yahoo.com>
2011-03-20 22:45:54 +0100

    Fix linux compilation
    Revert (most of) androidconfigbuild.sh, mkspecs/android-g++/qmake.conf
    Cleanup "configure"

--- androidconfigbuild.sh
+++ androidconfigbuild.sh
@@ -1,36 +1,30 @@
-#!/bin/bash
+#!/bin/sh
 
-echo $0 $@
+#default values
 
-NDK_TOOLCHAIN_PREFIX=arm-linux-androideabi
-NDK_TOOLCHAIN_VERSION=4.4.3
-NDK_PLATFORM=9
-export ANDROID_PLATFORM=$NDK_PLATFORM
-
-#defaults:
-CLEAN_QT=1
-CONFIGURE_QT=1
-BUILD_QT=1
-INSTALL_QT=1
-SHARED_QT=1
-EXCEPTIONS_QT=1
-DEBUG_QT=0
-RELEASE_QT=1
-MODIFY_DEST_DIR_QT=1
-PATCH_QT=0
+PLATFORM="-platform linux-g++"
+NDK_ROOT=/usr/local/android-ndk-r5
+NDK_HOST=linux-x86
 
 if [ "$OSTYPE" = "msys" ]; then
 	PLATFORM="-platform win32-g++"
-	DEST_DIR_QT=C:/Necessitas/Android/4.8.0
-else
-	PLATFORM="-platform linux-g++"
-	DEST_DIR_QT=/data/data/eu.licentia.necessitas.ministro/files/qt
+	NDK_ROOT=c:/android/android-ndk-r5b
+	NDK_HOST=windows
 fi
 
-SRC_DIR_QT=`dirname $0`
-pushd .
-SRC_DIR_QT=`(cd "$SRC_DIR_QT"; /bin/pwd)`
-popd
+NDK_TOOLCHAIN_PREFIX=arm-linux-androideabi
+NDK_TOOLCHAIN_VERSION=4.4.3
+TARGET_ARCH=armeabi
+          # armeabi - tune for android arm v5
+          # armeabi-v7a - tune for android arm v7
+
+CONFIGURE_QT=0
+PATCH_QT=1
+COMPILATION_TYPE=1
+
+QT_INSTALL_DIR=/data/data/eu.licentia.necessitas.ministro/files/qt
+QT_SRC_DIR=`dirname $0`
+QT_SRC_DIR=`(cd "$QT_SRC_DIR"; /bin/pwd)`
 
 help()
 {
@@ -38,75 +32,42 @@ cat << EOF
 usage: $0 options
 
 OPTIONS:
-   -p      Shows this message
-   -l      Clean qt
-   -q      Qt build options
-                   0 - don't configure qt (only compile) default
-                   1 - configure qt and compile qt
-   -b      Build qt
-                   0 - don't build
-                   1 - build
-   -s      Install qt
-                   0 - don't install
-                   1 - install
-   -c      Patch qt
-                   0 - don't patch qt (used to make the installer)
-                   1 - patch qt (default)
-   -n      NDK root. Default "$NDK_ROOT"
-   -o      NDK host. Default "$NDK_HOST"
-   -f      NDK toolchain prefix. Default "$NDK_TOOLCHAIN_PREFIX"
+   -help   Shows this message
+   -r      NDK root. Default "$NDK_ROOT"
+   -h      NDK host. Default "$NDK_HOST"
+   -p      NDK toolchain prefix. Default "$NDK_TOOLCHAIN_PREFIX"
    -v      NDK toolchain version. Default "$NDK_TOOLCHAIN_VERSION"
    -a      Target cpu architecture. Default "$TARGET_ARCH"
                    armeabi     - tune for android arm v5
                    armeabi-v7a - tune for android arm v7
-                   Optional name suffix: v5/v7
-   -h      Shared
-                   0 - Build static version of qt
-                   1 - Build shared version of qt
-   -x      Exceptions options
-                   0 - don't use exceptions (you won't be able to build qtcreator with such a qt though)
-                   1 - use exceptions
-                   Optional name suffix: Xc/Nx
-   -d      Build debug qt
-                   Optional name suffix: D/R/DaR
-   -r      Build release qt
-                   Optional name suffix: R/D/DaR
-   -m      Modify install path according to passed in options (name suffixes applied in above listed order).
-
-   -i      Install path. Default "$DEST_DIR_QT"
+   -q      Qt build options.
+                   0 - don't configure qt (only compile) default
+                   1 - configure qt and compile qt
+   -d      Build type. Default "$COMPILATION_TYPE"
+                   1 - Compile and link Qt with debugging turned on.
+                   0 - Compile and link Qt using release compilation flags, it also
+                   contains debugging informations.
+   -c      Patch qt.
+                   0 - don't patch qt (used to make the installer)
+                   1 - patch qt (default)
+   -i      Install path. Default "$QT_INSTALL_DIR"
 EOF
 }
 
-INSTSUFFIX=""
-CFGOPTIONS=""
-while getopts "p:l:q:b:k:c:n:o:f:v:a:h:x:d:r:m:i:" arg; do
+
+while getopts "help:r:h:p:v:a:q:c:i:d:" arg; do
 case $arg in
-	p)
+	help)
 		help
 		exit 0
 		;;
-	l)
-		CLEAN_QT=$OPTARG
-		;;
-	q)
-		CONFIGURE_QT=$OPTARG
-		;;
-	b)
-		BUILD_QT=$OPTARG
-		;;
-	k)
-		INSTALL_QT=$OPTARG
-		;;
-	c)
-		PATCH_QT=$OPTARG
-		;;
-	n)
+	r)
 		NDK_ROOT=$OPTARG
 		;;
-	o)
+	h)
 		NDK_HOST=$OPTARG
 		;;
-	f)
+	p)
 		NDK_TOOLCHAIN_PREFIX=$OPTARG
 		;;
 	v)
@@ -115,37 +76,17 @@ case $arg in
 	a)
 		TARGET_ARCH=$OPTARG
 		;;
-	h)
-		SHARED_QT=$OPTARG
-		if [ "$SHARED_QT" -eq "1" ]; then
-			INSTSUFFIX="${INSTSUFFIX}Sh"
-			CFGOPTIONS="${CFGOPTIONS} -shared "
-		else
-			INSTSUFFIX="${INSTSUFFIX}St"
-			CFGOPTIONS="${CFGOPTIONS} -static "
-		fi
+	q)
+		CONFIGURE_QT=$OPTARG
 		;;
-	x)
-		EXCEPTIONS_QT=$OPTARG
-		if [ "$EXCEPTIONS_QT" -eq "1" ]; then
-			INSTSUFFIX="${INSTSUFFIX}Xc"
-			CFGOPTIONS="${CFGOPTIONS} -exceptions "
-		else
-			INSTSUFFIX="${INSTSUFFIX}Nx"
-			CFGOPTIONS="${CFGOPTIONS} -no-exceptions "
-		fi
-		;;
-	d)
-		DEBUG_QT=$OPTARG
-		;;
-	r)
-		RELEASE_QT=$OPTARG
+	c)
+		PATCH_QT=$OPTARG
 		;;
 	i)
-		DEST_DIR_QT=$OPTARG
+		QT_INSTALL_DIR=$OPTARG
 		;;
-	m)
-		MODIFY_DEST_DIR_QT=$OPTARG
+	d)
+		COMPILATION_TYPE=$OPTARG
 		;;
 	?)
 		help
@@ -154,30 +95,6 @@ case $arg in
 esac
 done
 
-if [ "$DEBUG_QT" -eq "1" -a "$RELEASE_QT" -eq "1" ]; then
-	# This doesn't work, the release and debug builds use the same
-	# temporary folders and end up with a release lib/dll that's really
-	# a debug one.
-	CFGOPTIONS="${CFGOPTIONS} -debug-and-release "
-	INSTSUFFIX="${INSTSUFFIX}DaR"
-else
-	if [ "$DEBUG_QT" -eq "1" ]; then
-		CFGOPTIONS="${CFGOPTIONS} -debug "
-		INSTSUFFIX="${INSTSUFFIX}D"
-	else
-		CFGOPTIONS="${CFGOPTIONS} -release "
-		INSTSUFFIX="${INSTSUFFIX}R"
-	fi
-fi
-
-if [ "$MODIFY_DEST_DIR_QT" -eq "1" ]; then
-	DEST_DIR_QT=${DEST_DIR_QT}-${INSTSUFFIX}
-else
-	echo "Please sepcify -m 1 to $0 to prevent configuration pollution. Exiting."
-	exit 1
-fi
-
-echo "New install of Qt will be to $DEST_DIR_QT"
 
 # Please set the following evn vars correctly !!!
 export ANDROID_NDK_ROOT=$NDK_ROOT
@@ -196,36 +113,11 @@ ANDROID_NDK_PLATFORM=android-$NDK_PLATFORM
 ANDROID_TARGET_ARCH=$TARGET_ARCH
 EOF
 
-# Without this, make will not be able to translate relative paths
-# properly as it can't step beyond where / is mounted.
-if [ "$OSTYPE" = "msys" ]; then
-	MAKEDIR=`pwd -W`
-	MAKEFILE=$MAKEDIR/Makefile
-	MOCABLESMAKEFILE=$MAKEDIR/src/corelib/Makefile
-else
-	MAKEFILE=Makefile
-	MOCABLESMAKEFILE=Makefile
-fi
-
-if [ "$CLEAN_QT" -eq "1" ]; then
-	if [ -f Makefile ]; then
-		make -f $MAKEFILE distclean
-	fi
-	if [ -d qmake ]; then
-		pushd .
-		cd qmake
-		if [ -f Makefile ]; then
-			make -f $MAKEFILE clean
-		fi
-		popd
-	fi
-fi
-
 if [ $CONFIGURE_QT = 1 ]
 then
-	if [ $SRC_DIR_QT = $PWD ]
+	if [ $QT_SRC_DIR = $PWD ]
 	then
-		make -f $MAKEFILE confclean distclean
+		make confclean distclean
 		rm -fr include
 		rm -fr plugins
 		rm -fr lib
@@ -234,51 +126,27 @@ then
 		git checkout imports
 	fi
 
-	$SRC_DIR_QT/configure -v -opensource -qpa -arch arm \
+	CTYPE=-debug
+	if [ $COMPILATION_TYPE = 0 ]
+	then
+		$CTYPE= -release -reduce-relocations -reduce-exports 
+	fi
+	
+	$QT_SRC_DIR/configure -v -opensource $CTYPE -qpa -arch arm \
 		-no-phonon -freetype -fast -xplatform android-g++ \
 		$PLATFORM -host-little-endian \
 		-little-endian -no-qt3support -no-largefile \
-		-openssl -pch -reduce-relocations -reduce-exports \
-		-nomake demos -nomake examples -confirm-license \
-		$CFGOPTIONS -prefix $DEST_DIR_QT \
-		-script || exit 1
+		--prefix=$QT_INSTALL_DIR \
+		-openssl -shared -pch \
+		-nomake demos -nomake examples -confirm-license -exceptions \
+		-webkit -script || exit 1
 fi
 
-# This should loop until make succeeds, Workaround for Cygwin/MSYS
-# couldn't commit heap memory error
-if [ "$BUILD_QT" = "1" ]; then
-	make -f $MAKEFILE -j 9
-	while [ "$?" != "0" ]
-	do
-		if [ -f /usr/break-make ]; then
-			echo "Detected break-make"
-			rm -f /usr/break-make
-			exit 1
-		fi
-		make -f $MAKEFILE -j 9
-	done
+make -j3 || exit 1
 
-	if [ $PATCH_QT = 1 ]
-	then
-	    if [ "$OSTYPE" = "msys" ]; then
-	        $SRC_DIR_QT/qpatch.exe $SRC_DIR_QT/files-to-patch-android-mingw $DEST_DIR_QT $PWD
-	    else
-	        $SRC_DIR_QT/qpatch $SRC_DIR_QT/files-to-patch-android $DEST_DIR_QT $PWD
-	    fi
-	fi
+if [ $PATCH_QT = 1 ]
+then
+    $QT_SRC_DIR/qpatch $QT_SRC_DIR/files-to-patch-android $QT_INSTALL_DIR $PWD
 fi
 
 #INSTALL_ROOT=$QT_SRC_DIR/qt/$TARGET_ARCH make install
-if [ "$INSTALL_QT" = "1" ] ; then
-	make -f $MAKEFILE install
-	while [ "$?" != "0" ]
-	do
-		if [ -f /usr/break-make ]; then
-			echo "Detected break-make"
-			rm -f /usr/break-make
-			exit 1
-		fi
-		make -f $MAKEFILE install
-	done
-	$SRC_DIR_QT/copy-private-headers.sh include $DEST_DIR_QT/private-headers
-fi
