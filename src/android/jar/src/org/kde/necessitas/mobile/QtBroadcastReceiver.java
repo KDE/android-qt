250949387a0dc5f7d6e3af0e9c5c105040c8144f
BogDan Vatra <bog_dan_ro@yahoo.com>
2012-05-24 13:00:00 +0300

    Qt Android BroadcastReceiver wapper.
    Android BroadcastReceiver Java class into main Jar file to be loaded by Ministro.
    
    Although it is very generic, C++ implementation is currently in Connectivity
    module until a good way of making it public in main Qt repository is found.
    
    I moved this file to mobility package, because I'm planning to split the
    mobility and main packages into separate .jar files, and because this is used
    only by mobility.
    
    Many thanks to Lauri Laanmets <lauri.laanmets@proekspert.ee> for this work

---
+++ src/android/jar/src/org/kde/necessitas/mobile/QtBroadcastReceiver.java
@@ -0,0 +1,43 @@
+/*
+    Copyright (c) 2011-2012, Lauri Laanmets <lauri.laanmets@proekspert.ee>
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+        * Redistributions of source code must retain the above copyright
+        notice, this list of conditions and the following disclaimer.
+        * Redistributions in binary form must reproduce the above copyright
+        notice, this list of conditions and the following disclaimer in the
+        documentation and/or other materials provided with the distribution.
+        * Neither the name of Lauri Laanmets (AS Proekspert) nor the
+        names of its contributors may be used to endorse or promote products
+        derived from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY Lauri Laanmets (AS Proekspert) ''AS IS'' AND ANY
+    EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+    WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL Lauri Laanmets (AS Proekspert) BE LIABLE FOR ANY
+    DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+    ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+package org.kde.necessitas.mobile;
+
+import android.content.BroadcastReceiver;
+import android.content.Intent;
+import android.content.Context;
+
+public class QtBroadcastReceiver extends BroadcastReceiver{
+    /* Pointer to the Qt object that "owns" the Java object */
+    int qtObject=0;
+
+    public void onReceive(Context context, Intent intent){
+        jniOnReceive(qtObject, context, intent);
+    }
+
+    public native void jniOnReceive(int qtObject, Context context, Intent intent);
+}
